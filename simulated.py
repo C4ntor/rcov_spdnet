86b7340d40a53d7bcdab17e27a0c91dd9309098a
Michele Palma <116038756+C4ntor@users.noreply.github.com>
2024-06-11 22:53:11 +0200

    fix: small bug fix

--- simulated.py
+++ simulated.py
@@ -14,7 +14,7 @@ from spdnet.net.optimizer import StiefelMetaOptimizer
 #ARGS
 N_OBS=1000
 N_STOCK = 2
-N_LAGS = 5
+N_LAGS = 4
 SEED = 1
 
 torch.manual_seed(SEED)
@@ -28,10 +28,10 @@ train_data = [make_spd_matrix(N_STOCK).tolist() for _ in range(N_OBS)]
 
 
 dict = extract_x_y_from_tseries(train_data,N_LAGS,'d')
-#preprocess the generated matrices, preparing them to be included in RCOVDataset (as x, and y)
+#preprocess the generated matrices, using BLOCK DIAGONAL MODE, preparing them to be included in RCOVDataset (as x, and y)
 
 
-#PLEASE NOTICE THAT KRONECKER PRODUCT MODE IS STILL EXPERIMENTAL!
+#PLEASE NOTICE THAT KRONECKER PRODUCT MODE ('k') IS STILL EXPERIMENTAL!
 train_dataset = RCOVDataset(dict['x'], dict['y'])
 
 
@@ -51,29 +51,32 @@ optimizer = StiefelMetaOptimizer(optimizer)
 criterion = MSELoss()
 loss_hist = []
 i=0
-h_real_var_01 = []              #
-h_pred_var_01 = []              #
-h_dumb_var_01 = []
+baseline_pred = None
+time_step = N_LAGS-1
 cumulative_loss=0
+mse_basel= 0
+loss_baseline = []
+var_00_pred = []
+var_00_true = []
 
 for x, y  in train_dataset:
         x_i = torch.Tensor(x)
         assert(is_spd(x_i.detach().numpy()))
         y_i = torch.Tensor(y)
         assert(is_spd(y_i.detach().numpy()))
-
-        h_real_var_01.append(y_i[0][1].item())         #
-
+        baseline_pred = torch.Tensor(train_data[time_step])
+        var_00_true.append(y_i[0][0].detach().numpy())
         optimizer.zero_grad()
         prediction = network(x_i)
         assert(is_spd(prediction.detach().numpy()))
-        h_pred_var_01.append(prediction[0][1].item())  #
-        h_dumb_var_01.append(dict['b'][i])
-        print(dict['b'][i])
+        var_00_pred.append(prediction[0][0].detach().numpy())
         print('x_i',x_i)
         print('y_i',y_i)
+        print('y_i_basel_PRED', baseline_pred)
         print('y_i_PRED',prediction)
 
+        mse_basel = torch.mean((baseline_pred - y_i) ** 2)
+        loss_baseline.append(mse_basel)
         loss = criterion(prediction, y_i)
         loss_i = loss.item()
         loss.backward()
@@ -81,27 +84,29 @@ for x, y  in train_dataset:
         print("LOSS: {0}".format(loss_i))
 
         
-
+        time_step += 1
         loss_hist.append((loss_i))
         optimizer.step()
 
 torch.save(network.state_dict(), 'model.pth')
 
 
-plt.plot(loss_hist, label='loss')
+plt.plot(loss_hist, label='loss_network')
+plt.plot(loss_baseline, label = 'loss_baseline')
 plt.legend()
 plt.show()
 
-#we can load it as follows: loaded_model = RiemSPD(M_SIZE,N_STOCK)
-                        #loaded_model.load_state_dict(torch.load('model.pth'))
-
-plt.plot(h_pred_var_01, 'b', label='predicted_var_01') #
-plt.plot(h_real_var_01, 'r', label='real_var_01') #
-
+#we can load it as follows: loaded_model = RiemSPD(M_SIZE,N_STOCK
+#loaded_model.load_state_dict(torch.load('model.pth'))
+plt.plot(var_00_true, label='var_00 true')
+plt.plot(var_00_pred, label = 'var_00 predicted')
 plt.legend()
 plt.show()
-print("CUMULATIVE_LOSS:", sum(loss_hist))
-print("AVG_LOSS:", sum(loss_hist)/N_OBS)
+print("CUMULATIVE_LOSS_NET:", sum(loss_hist))
+print("AVG_LOSS_NET:", sum(loss_hist)/N_OBS)
+print("CUMULATIVE_LOSS_BASELINE:", sum(loss_baseline))
+print("AVG_LOSS_BASELINE:", sum(loss_baseline)/N_OBS)
+
 
 
 
